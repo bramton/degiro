1afc264d9bedac0488dd50a16cd9ff2d44abc372
Bram <bram@baozi>
2017-11-28 10:19:30 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# degiro
-Very crude unofficial Python API for DeGiro
+# Unofficial DeGIRO Python API
+Very basic **unofficial** Python API for [DeGiro](https://www.degiro.nl). This API is only able to get details about your portfolio. It cannot be used for automatic trading. For a way more extensive Node.js API have a look at [https://github.com/pladaria/degiro](pladarias) work.
+
+:warning: DeGiro could change their API at any moment, if something is not working, please open an issue.
+
+# Security
+Your password is stored plain text in a JSON file. Take adequate measures !, e.g. `chmod` it to `600`.
+
+# Example usage
+```python
+from degiro import degiro
+
+la = degiro()
+la.login('config.json')
+la.getConfig()
+pfs = la.getPortfolioSummary()
+portfolio = la.getPortfolio()
+total = pfs['equity']
+
+# Prints a pretty table of your equities and their allocation.
+print('{:<20}\tsize\tvalue\tsubtot\t\talloc'.format('Product'))
+for row in portfolio:
+    subtot = row['size']*row['price']
+    alloc = (subtot/total)*100 # Asset allocation (%)
+    print('{:<20}\t{:3d}\t{:6.2f}\t{:7.2f}\t\t{:2.1f}%'.format(row['product'], row['size'], row['price'], subtot, alloc))
+print('Total: {:.2f}'.format(total))
+```
---
+++ degiro.py
@@ -0,0 +1,92 @@
+import requests
+import json
+
+class degiro:
+    def __init__(self):
+        self.user = dict()
+        self.data = None
+    
+    def login(self, conf_path):
+        conf = json.load(open(conf_path))
+        self.sess = requests.Session()
+        
+        # Login
+        url = 'https://trader.degiro.nl/login/secure/login'
+        payload = {'username': conf['username'],
+                   'password': conf['password'],
+                   'isPassCodeReset': False,
+                   'isRedirectToMobile': False}
+        header={'content-type': 'application/json'}
+        r = self.sess.post(url, data=json.dumps(payload))
+        
+        # Get session id
+        self.sessid = r.headers['Set-Cookie']
+        self.sessid = self.sessid.split(';')[0]
+        self.sessid = self.sessid.split('=')[1]
+
+        print('Login')
+        print('\tStatus code: {}'.format(r.status_code))
+        print('\tSession id: {}'.format(self.sessid))
+        
+    # This contain loads of user data, main interest here is the 'intAccount'
+    def getConfig(self):
+        url = 'https://trader.degiro.nl/pa/secure/client'
+        payload = {'sessionId': self.sessid}
+
+        r = self.sess.get(url, params=payload)
+        data = r.json()
+        self.user['intAccount'] = data['intAccount']
+        
+        print('Get config')
+        print('\tStatus code: {}'.format(r.status_code))
+        print('\tAccount id: {}'.format(self.user['intAccount']))
+        
+    # This gets a lot of data, orders, news, portfolio, currencies etc.
+    def getData(self):
+        url = 'https://trader.degiro.nl/trading/secure/v5/update/'
+        url += str(self.user['intAccount'])+';'
+        url += 'jsessionid='+self.sessid
+        payload = {'portfolio': 0,
+                   'totalPortfolio': 0,
+                   'orders': 0,
+                   'historicalOrders': 0,
+                   'transactions': 0,
+                   'alerts': 0,
+                   'cashFunds': 0,
+                   'intAccount': self.user['intAccount'],
+                   'sessionId': self.sessid}
+
+        r = self.sess.get(url, params=payload)
+        self.data = r.json()
+        print('Get data')
+        print('\tStatus code: {}'.format(r.status_code))
+    
+    # Only returns a summary of the portfolio
+    def getPortfolioSummary(self):
+        if self.data == None:
+            self.getData()
+        resv = dict()
+        for v in self.data['totalPortfolio']['value']:
+            resv[v['name']] = v['value']
+
+        pfSummary = dict()
+        pfSummary['equity'] = resv['portVal']
+        pfSummary['cash'] = resv['cash']
+        pfSummary['total'] = resv['total']
+        return pfSummary
+    
+    # Returns the entire portfolio
+    def getPortfolio(self):
+        portfolio = []
+        for row in self.data['portfolio']['value']:
+            entry = dict()
+            for y in row['value']:
+                k = y['name']
+                v = None
+                if 'value' in y:
+                    v = y['value']
+                entry[k] = v
+            # Also historic equities are returned, let's omit them
+            if entry['size'] != 0:
+                portfolio.append(entry)
+        return portfolio
